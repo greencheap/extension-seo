bf233e796fd169dff34dd4f7b78aade41027fe29
ynswtf <yunussenem@icloud.com>
2020-08-12 20:01:59 +0300

    Scriptsphp udpate

--- scripts.php
+++ scripts.php
@@ -19,9 +19,12 @@ return [
         }
     },
 
-    'update' => function($app)
-    {
-
-    }
+    'update' => [
+        '1.0.4' => function($app){
+            $path = App::get('path');
+            if(!$app['file']->exists("$path/robots.txt")){
+                $app['file']->copy("$path/packages/greencheap/seo/.robots" , "$path/robots.txt");
+            }
+        }
+    ]
 ];
-?>
