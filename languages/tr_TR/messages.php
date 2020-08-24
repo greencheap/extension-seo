da0c94e8c3e0891e6617f6c4ba577f2eb4f92928
ynswtf <yunussenem@icloud.com>
2020-08-24 03:54:17 +0300

    1.0.4

---
+++ languages/tr_TR/messages.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'Seo Settings' => 'Seo Ayarları',
+  'Save' => 'Kaydet',
+  'Subject' => 'Konu',
+  'Title' => 'Başlık',
+  'Description' => 'Açıklama',
+  'This plugin is developed according to the protocol specified in' => 'Bu eklenti, içinde belirtilen protokole göre geliştirilmiştir.',
+  'Not Found Extension %ex%' => 'Uzantı Bulunamadı %ex%',
+  'Something is wrong.' => 'Bir şey yanlış.',
+);
